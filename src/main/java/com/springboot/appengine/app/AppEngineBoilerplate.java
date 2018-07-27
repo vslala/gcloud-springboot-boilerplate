c10f01b7868ca6f9d1c68204705a9834f3d90463
varunshr <varunshrivastava@globalpay.com>
2018-07-27 14:02:38 +0000

    add application file

---
+++ src/main/java/com/springboot/appengine/app/AppEngineBoilerplate.java
@@ -0,0 +1,12 @@
+package com.springboot.appengine.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppEngineBoilerplate {
+
+	public static void main(String [] args) {
+		SpringApplication.run(AppEngineBoilerplate.class, args);
+	}
+}
